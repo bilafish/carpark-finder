c6c7ca48df80b898493e603d2d5deefe8eb68bb5
Jason <bilafish@gmail.com>
2020-07-03 11:47:29 +0800

    :alembic: Set cookie

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { CookiesProvider } from "react-cookie";
 
-ReactDOM.render(<App />, document.getElementById('map'));
+ReactDOM.render(
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>,
+  document.getElementById("map")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
